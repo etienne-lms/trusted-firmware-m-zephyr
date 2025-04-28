aa930688337a8112d032780ec6ff366e29892dd6
Etienne Carriere <etienne.carriere@st.com>
2025-04-28 12:17:22 +0200

    Platform: STM32WBA65I_DK: update flash layout
    Update stm32wba65i_dk flash layout to allocate 256kB for TF-M
    runtime image, allocate 512kB to non-secure image and locate the
    secondary images right after the primary image instead of
    flash bank 2 base address to prevent wasting flash memory between
    area 1 and area 2.
    
    Update flash layout inline description comment accordingly.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@st.com>

--- platform/ext/target/stm/stm32wba65i_dk/partition/flash_layout.h
+++ platform/ext/target/stm/stm32wba65i_dk/partition/flash_layout.h
@@ -27,12 +27,11 @@
  *  0x001_8000 OTP / NV counters area (16 KB)
  *  0x001_c000 Protected Storage Area (16 KB)
  *  0x002_0000 Internal Trusted Storage Area (16 KB)
- *  0x002_4000 Secure primary image (384 KB)
- *  0x008_4000 Non-secure primary image (384 KB)
- *  0x00e_4000 Non-secure private storage (112 KB)
- *  0x010_0000 Secure secondary image (384 KB)
- *  0x016_0000 Non-secure secondary image (384 KB)
- *  0x01c_0000 Non-secure private storage #2 (256 KB)
+ *  0x002_4000 Secure primary image (256 KB)
+ *  0x006_4000 Non-secure primary image (512 KB)
+ *  0x00e_4000 Secure secondary image (256 KB)
+ *  0x012_4000 Non-secure secondary image (512 KB)
+ *  0x01a_4000 Non-secure private storage (up to 368 KB)
  *
  * Note:
  * Secure primary image, Non-secure primary image,
@@ -125,7 +124,7 @@
 
 /* Secure image secondary slot */
 #define FLASH_AREA_2_ID            (FLASH_AREA_1_ID + 1)
-#define FLASH_AREA_2_OFFSET        (FLASH_NV_COUNTERS_AREA_OFFSET + FLASH_B_SIZE)
+#define FLASH_AREA_2_OFFSET        (FLASH_AREA_1_OFFSET + FLASH_AREA_1_SIZE)
 #define FLASH_AREA_2_SIZE          (FLASH_S_PARTITION_SIZE)
 /* Non-secure image secondary slot */
 #define FLASH_AREA_3_ID            (FLASH_AREA_2_ID + 1)
@@ -155,9 +154,9 @@
 /*TFM_PARTITION_FIRMWARE_UPDATE*/
 
 /* The size of S partition */
-#define FLASH_S_PARTITION_SIZE          (0x60000) /* 384 KB for S partition */
+#define FLASH_S_PARTITION_SIZE          (0x40000) /* 256 KB for S partition */
 /* The size of NS partition */
-#define FLASH_NS_PARTITION_SIZE         (0x60000) /* 384 KB for NS partition */
+#define FLASH_NS_PARTITION_SIZE         (0x80000) /* 512 KB for NS partition */
 
 /* Non Volatile Counters definitions */
 #define FLASH_NV_COUNTERS_AREA_OFFSET           (0x0000)
