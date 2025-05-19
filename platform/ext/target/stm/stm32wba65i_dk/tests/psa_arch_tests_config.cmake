f6323a762a2dd7c4a7a4f4fe39083c988cbd0aa9
Etienne Carriere <etienne.carriere@foss.st.com>
2025-05-19 14:59:43 +0200

    Platform: STM32WBA65I_DK: explicitly use L562E_DK for PSA Arch tests
    Explicitly state that stm32wba65i_dk platform rely on PSA Arch Tests
    integration for platform stm32l562e_dk without watchdog availability.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

--- platform/ext/target/stm/stm32wba65i_dk/tests/psa_arch_tests_config.cmake
+++ platform/ext/target/stm/stm32wba65i_dk/tests/psa_arch_tests_config.cmake
@@ -1,7 +1,9 @@
 #-------------------------------------------------------------------------------
-# Copyright (c) 2023 STMicroelectronics. All rights reserved.
+# Copyright (c) 2025 STMicroelectronics. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
-#
 #-------------------------------------------------------------------------------
-set(PSA_API_TEST_TARGET stm32l562e_dk)
+
+# Intentionally use stm32l562e_dk without watchdog support
+set(PSA_API_TEST_TARGET "stm32l562e_dk")
+set(WATCHDOG_AVAILABLE 0)
